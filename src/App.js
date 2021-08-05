a7f660bd9860f2ee613622c3cc14974da5c73d3f
Leon Erasmus <leon@santiagobiltong.cl>
2021-08-04 22:08:43 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React from "react";
+
+//include images into your bundle
+import { useState } from "react";
+
+
+import './App.css';
+
+const TrafficLight = () => {
+	const [color, setColor] = useState({
+		color1: true,
+		color2: false,
+		color3: false
+	});
+
+	return (
+		<>
+			<div className="container-fluid">
+				<div className="row">
+					<div className="mx-auto mt-5">
+						<div className="box">
+							<div
+								className={
+									color.color1 ? "color1-light" : "color1"
+								}
+								onClick={() =>
+									setColor({
+										color: false,
+										color1: true,
+										color2: false,
+										color3: false
+									})
+								}></div>
+							<div
+								className={
+									color.color2 ? "color2-light" : "color2"
+								}
+								onClick={() =>
+									setColor({
+										color: false,
+										color1: false,
+										color2: true,
+										color3: false
+									})
+								}></div>
+							<div
+								className={
+									color.color3 ? "color3-light" : "color3"
+								}
+								onClick={() =>
+									setColor({
+										color: false,
+										color1: false,
+										color2: false,
+										color3: true
+									})
+								}></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default TrafficLight;
+
